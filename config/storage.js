101f596c8ae8cf191d56cf3d764fb18eba452ae4
AlteonRea <alteonreastra@gmail.com>
2024-12-06 19:51:59 +0700

    add img to registration

--- config/storage.js
+++ config/storage.js
@@ -1,6 +1,6 @@
 const { Storage } = require('@google-cloud/storage');
 const service_account = require('../owner-sa-key.json');
 
-const storage = new Storage({ credentials: service_account });
+const storage = new Storage();
 
 module.exports = storage;
