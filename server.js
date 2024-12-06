4d0218fabfff95f295aa482f930b24242d708c40
AlteonRea <alteonreastra@gmail.com>
2024-12-06 20:03:05 +0700

    add img to registration

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-// server.js
-const app = require('./app'); // Import the configured app
-const PORT = process.env.PORT || 3000;
+const app = require('./app');
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
